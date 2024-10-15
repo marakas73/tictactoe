ad27b064c73237a5c3f6336164b2f9cd2ef2aba3
marakas73 <ysehgg@gmail.com>
2024-10-15 16:08:08 +0300

    Update forbidden exception throwing

--- src/main/kotlin/com/test/tictactoe/controller/user/UserHttpController.kt
+++ src/main/kotlin/com/test/tictactoe/controller/user/UserHttpController.kt
@@ -4,6 +4,7 @@ import com.test.tictactoe.controller.user.dto.GameRecordDTO
 import com.test.tictactoe.controller.user.response.UserInfoResponse
 import com.test.tictactoe.exception.throwCannotGetGameHistoryException
 import com.test.tictactoe.exception.throwCannotGetUserInfoException
+import com.test.tictactoe.exception.throwForbidden
 import com.test.tictactoe.service.TokenService
 import com.test.tictactoe.service.UserService
 import com.test.tictactoe.utils.toGameRecordDTO
@@ -24,7 +25,7 @@ class UserHttpController(
         @RequestHeader("Authorization") authHeader: String
     ): List<GameRecordDTO> {
         val token = authHeader.substringAfter("Bearer ")
-        val login = tokenService.extractLogin(token)?: throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        val login = tokenService.extractLogin(token)?: throwForbidden()
 
         return userService.getGameHistory(
             login = login
@@ -40,7 +41,7 @@ class UserHttpController(
         @RequestHeader("Authorization") authHeader: String
     ): UserInfoResponse {
         val token = authHeader.substringAfter("Bearer ")
-        val login = tokenService.extractLogin(token)?: throw ResponseStatusException(HttpStatus.FORBIDDEN)
+        val login = tokenService.extractLogin(token)?: throwForbidden()
 
         return userService.getUserInfo(
             login = login
