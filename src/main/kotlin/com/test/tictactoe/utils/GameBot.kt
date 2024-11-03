9ae9f38238dcb03efacb1d11be03a6d988b5b0e0
marakas73 <ysehgg@gmail.com>
2024-11-03 19:59:18 +0300

    WIP changing to negamax

--- src/main/kotlin/com/test/tictactoe/utils/GameBot.kt
+++ src/main/kotlin/com/test/tictactoe/utils/GameBot.kt
@@ -6,134 +6,103 @@ import com.test.tictactoe.model.Game
 import kotlin.math.max
 import kotlin.math.min
 
-class GameBot {
-    companion object {
-        private const val DEFAULT_MAX_DEPTH = 10
+object GameBot {
+    private const val START_DEPTH = 2
+    private const val END_DEPTH = 8
 
-        fun getOptimalMove(
-            game: Game,
-            depth: Int = DEFAULT_MAX_DEPTH
-        ): Pair<Int, Int> {
-            val optimalMoves: MutableList<Pair<Int, Int>> = mutableListOf()
-            var currentMaxMoveValue = Int.MIN_VALUE
+    fun getBestMove(game: Game): Move {
 
-            val field = game.field.field
-            for (rowIndex in field.indices) {
-                for (colIndex in field[rowIndex].indices) {
-                    // If cell isn't empty
-                    if (field[rowIndex][colIndex] != null)
-                        continue;
+        // Run a depth increasing search
+        val bestMove = iterativeDeepening(game, START_DEPTH, END_DEPTH)
+        return bestMove
+    }
 
-                    // Temp move
-                    field[rowIndex][colIndex] = game.memberSymbol
-                    val moveValue = minimax(
-                        colIndex,
-                        rowIndex,
-                        alpha = Int.MIN_VALUE,
-                        beta = Int.MAX_VALUE,
-                        isBotTurn = false,
-                        depth = depth,
-                        game = game
-                    )
-                    // Remove temp move
-                    field[rowIndex][colIndex] = null
-
-                    if (moveValue > currentMaxMoveValue) {
-                        currentMaxMoveValue = moveValue
-                        optimalMoves.clear()
-                        optimalMoves.add(Pair(colIndex, rowIndex))
-                    } else if (moveValue == currentMaxMoveValue) {
-                        optimalMoves.add(Pair(colIndex, rowIndex))
-                    }
-                }
-            }
-            return optimalMoves.random()
-        }
-
-        private fun minimax(
-            x: Int,
-            y: Int,
-            depth: Int,
-            alpha: Int,
-            beta: Int,
-            isBotTurn: Boolean,
-            game: Game
-        ): Int {
-            if (depth == 0)
-            {
-                return 0
-        }
-            val gameResult = checkGameResult(game, x, y, !isBotTurn)
-            if (gameResult != null)
-                return gameResult
-
-
-            val field = game.field.field;
-            if (isBotTurn) {
-                var maxEval = Int.MIN_VALUE
-
-                for (rowIndex in field.indices) {
-                    for (colIndex in field[rowIndex].indices) {
-                        // If cell isn't empty
-                        if (field[rowIndex][colIndex] != null)
-                            continue
-
-                        // Temp move
-                        field[rowIndex][colIndex] = game.memberSymbol
-                        val eval = minimax(
-                            x = colIndex,
-                            y = rowIndex,
-                            depth = depth - 1,
-                            alpha = alpha,
-                            beta = beta,
-                            isBotTurn = false,
-                            game = game
-                        )
-                        // Remove temp move
-                        field[rowIndex][colIndex] = null
-
-                        maxEval = max(maxEval, eval)
-
-                        val newAlpha = max(alpha, eval)
-                        if (beta <= newAlpha)
-                            return maxEval
-                    }
-                }
-
-                return maxEval
-            } else {
-                var minEval = Int.MAX_VALUE
-
-                for (rowIndex in field.indices) {
-                    for (colIndex in field[rowIndex].indices) {
-                        // If cell isn't empty
-                        if (field[rowIndex][colIndex] != null)
-                            continue;
-
-                        // Temp move
-                        field[rowIndex][colIndex] = game.ownerSymbol
-                        val eval = minimax(
-                            x = colIndex,
-                            y = rowIndex,
-                            depth = depth - 1,
-                            alpha = alpha,
-                            beta = beta,
-                            isBotTurn = true,
-                            game = game
-                        )
-                        // Remove temp move
-                        field[rowIndex][colIndex] = null
-
-                        minEval = min(minEval, eval)
-                        val newBeta = min(beta, eval)
-                        if (newBeta <= alpha)
-                            return minEval
-                    }
-                }
-
-                return minEval
+    private fun iterativeDeepening(game: Game, startDepth: Int, endDepth: Int): Move {
+        var moves = getSortedPotentialMoves(game)
+        if (moves.size == 1) return moves[0]
+        for (i in startDepth..endDepth) {
+            try {
+                moves = getScoredMoves(game, moves, i)
+            } catch (e: InterruptedException) {
+                break
             }
         }
+        return moves[0]
+    }
+
+    private fun getScoredMoves(game: Game, moves: List<Move>, depth: Int): List<Move> {
+        val scoredMoves = mutableListOf<ScoredMove>()
+        var alpha = -11000
+        val beta = 11000
+        var best = Int.MIN_VALUE
+
+        for (move in moves) {
+            game.makeMove(move)
+
+            val score = -negamax(
+                game,
+                move,
+                depth - 1,
+                -beta,
+                -alpha
+            )
+            scoredMoves.add(
+                ScoredMove(
+                    move = move,
+                    score = score
+                )
+            )
+
+            game.undoMove(move)
+            if (score > best) best = score
+            if (best > alpha) alpha = best
+            if (best >= beta) break
+        }
+
+        scoredMoves.sortByDescending { it.score }
+
+        return scoredMoves.map { it.move }
+    }
+
+    private fun negamax(
+        game: Game,
+        move: Move,
+        depth: Int,
+        alpha: Int,
+        beta: Int
+    ): Int {
+        var newAlpha = alpha
+
+        if (depth == 0 || getWinner(game, move) != null) {
+            return Evaluator.evaluateField(game, move, depth)
+        }
+
+        var value: Int
+        var best = Int.MIN_VALUE
+
+        val moves = getSortedPotentialMoves(game)
+
+        for (currentMove in moves) {
+
+            game.makeMove(currentMove)
+            value = -negamax(game, currentMove, depth - 1, -beta, -newAlpha)
+            game.undoMove(currentMove)
+
+            if (value > best) {
+                best = value
+            }
+            if (best > newAlpha) newAlpha = best
+            if (best >= beta) {
+                break
+            }
+        }
+        return best
+    }
+
+
+    private fun getSortedPotentialMoves(game) {
+        val moves = listOf<Move>()
+    }
 
         private fun getPlayerSymbol(game: Game, isBot: Boolean): GameSymbol =
             if (isBot) game.memberSymbol else game.ownerSymbol
@@ -146,5 +115,5 @@ class GameBot {
                 else -> null
             }
         }
-    }
+
 }
---
+++ src/main/kotlin/com/test/tictactoe/utils/Move.kt
@@ -0,0 +1,6 @@
+package com.test.tictactoe.utils
+
+data class Move(
+    val x: Int,
+    val y: Int,
+)
